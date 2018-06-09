d559e080e776fc1306170240f2003e203ab5f451
bluedevil-oit <ed151@ed151-dev-01.oit.duke.edu>
2018-06-09 08:55:58 -0400

    merge migrations

---
+++ FEA2D/migrations/0004_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('FEA2D', '0003_auto_20180522_1540'),
+        ('FEA2D', '0003_auto_20180601_2152'),
+    ]
+
+    operations = [
+    ]
